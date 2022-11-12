074bee8c1303853100d5e46205158f0cb021567e
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-11-11 22:23:10 -0500

    remove a bunch more of the old stuff

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="astro/client" />
-interface ImportMetaEnv {
-  readonly CMS_TOKEN: string;
-}
+interface ImportMetaEnv {}
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
