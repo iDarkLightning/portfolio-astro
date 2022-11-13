5df9a09606c480da14ea09c7fd36ef7a12fde0dd
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-11-13 16:48:31 -0500

    fix not loading thing

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -55,11 +55,8 @@ export const Signature: Component = () => {
 
   onMount(() => {
     const observer = new IntersectionObserver((entries) => {
-      // Loop over the entries
       entries.forEach((entry) => {
-        // If the element is visible
         if (entry.isIntersecting) {
-          // Add the animation class
           entry.target.classList.add("signature");
         }
       });
