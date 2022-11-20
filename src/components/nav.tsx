bb0e4fc024fdf876e8fd0eb2f0827c38756ba74b
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-11-20 14:14:17 +0600

    fix li ul order

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -25,15 +25,16 @@ export const Nav: Component<{ currentRoute: string }> = (props) => (
     <nav class="rounded-[6rem] bg-[#111]">
       <ul class="flex">
         {routes.map((item) => (
-          <a
-            href={item.route}
+          <li
             class={clsx(
               "no-underline py-2 px-4 rounded-[6rem] lg:py-4 lg:px-8",
               item.route === props.currentRoute && "bg-[#222]"
             )}
           >
-            <li>{item.name}</li>
-          </a>
+            <a href={item.route} class="no-underline">
+              {item.name}
+            </a>
+          </li>
         ))}
       </ul>
     </nav>
