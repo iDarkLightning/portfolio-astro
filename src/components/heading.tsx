bdfff819c4dbbd41706428296978dccb73f839ff
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-11-21 04:59:05 +0600

    remove back link

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -1,16 +1,9 @@
-import {
-  Motion,
-  VariantDefinition,
-  AnimationOptionsWithOverrides,
-} from "@motionone/solid";
+import { Motion } from "@motionone/solid";
 import type { Component } from "solid-js";
 import { animate, initial, transition } from "../lib/animations";
 
 export const Heading: Component<{ heading: string; sub: string }> = (props) => (
   <Motion.div initial={initial} animate={animate} transition={transition(0)}>
-    <a href="/" class="no-underline" rel="prefetch">
-      <p class="text-sm uppercase font-mono ">{"<-"} Index</p>
-    </a>
     <h1 class="text-3xl font-bold">{props.heading}</h1>
     <p class="font-serif italic text-accent-300">{props.sub}</p>
   </Motion.div>
