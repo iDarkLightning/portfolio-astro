91a36481380fdf16306e05265f919a17e4213204
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-11-20 13:34:20 +0600

    apply for writing

--- src/components/writing.tsx
+++ src/components/writing.tsx
@@ -9,25 +9,34 @@ import type { Article } from "../types/article";
 
 export const ArticleCard: Component<{ item: Article; idx: number }> = (
   props
-) => (
-  <Motion.a
-    initial={initial}
-    animate={animate}
-    transition={transition(props.idx / 100 + (2 + props.idx))}
-    href={props.item.url}
-    class="w-full flex flex-col py-4 rounded-md transition-all no-underline gap-2"
-  >
-    <div class="w-full flex gap-2 justify-between">
-      <p class="font-semibold">{props.item.title}</p>
-      <p class="text-accent">
-        {props.item.date
-          .toLocaleDateString()
-          .replace(`/${props.item.date.getFullYear()}`, "")}
-      </p>
-    </div>
-    <p class="opacity-80 font-normal">{props.item.description}</p>
-  </Motion.a>
-);
+) => {
+  let ref: HTMLAnchorElement | null = null;
+
+  setTimeout(() => {
+    ref?.classList.add("transition-opacity");
+  }, 1500);
+
+  return (
+    <Motion.a
+      ref={(el) => (ref = el)}
+      initial={initial}
+      animate={animate}
+      transition={transition(props.idx / 100 + (2 + props.idx))}
+      href={props.item.url}
+      class="w-full flex flex-col py-4 rounded-md no-underline gap-2"
+    >
+      <div class="w-full flex gap-2 justify-between">
+        <p class="font-semibold">{props.item.title}</p>
+        <p class="text-accent">
+          {props.item.date
+            .toLocaleDateString()
+            .replace(`/${props.item.date.getFullYear()}`, "")}
+        </p>
+      </div>
+      <p class="opacity-80 font-normal">{props.item.description}</p>
+    </Motion.a>
+  );
+};
 
 export const ArticleYear: Component<{ year: string }> = (props) => (
   <Motion.p
