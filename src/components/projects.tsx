42fcc10bdff280e87b83a9854f72c5255109f2ce
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-11-20 13:32:33 +0600

    change interval ms again

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -10,7 +10,7 @@ export const ProjectCard: Component<{ project: Project; idx: number }> = (
 
   setTimeout(() => {
     ref?.classList.add("transition-opacity");
-  }, (props.idx / 100 + (2 + props.idx)) * 0.5);
+  }, 1500);
 
   return (
     <Motion.a
