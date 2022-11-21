4e003117b7426c4f1c1c14cb61c2c68b7d847bd3
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-11-21 09:45:44 +0600

    add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import mdx from "@astrojs/mdx";
-
+import robotsTxt from "astro-robots-txt";
 import prefetch from "@astrojs/prefetch";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nirjhor.dev",
   markdown: {
     shikiConfig: {
-      theme: "poimandres",
-    },
+      theme: "poimandres"
+    }
   },
-  integrations: [tailwind(), solidJs(), mdx(), prefetch()],
+  integrations: [tailwind(), solidJs(), mdx(), prefetch(), robotsTxt(), sitemap()]
 });
